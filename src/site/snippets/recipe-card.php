ec921064e6251b46ab121c77bbda72529d6a8268
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-05-10 19:37:34 +0200

    feat: fade in images once loaded

--- src/site/snippets/recipe-card.php
+++ src/site/snippets/recipe-card.php
@@ -12,9 +12,9 @@ if (isset($recipe)) {
 } ?>
 
 <div class="relative shadow rounded-card aspect-ratio-card" x-data="{ isFavorite: false }">
-  <div class="absolute flex items-end w-full h-full overflow-hidden rounded-card">
+  <div class="absolute flex items-end w-full h-full overflow-hidden rounded-card" x-data="{ loaded: false }">
     <?php if ($image) : ?>
-      <img src="<?= $image->thumb(['width' => 400])->url() ?>" class="absolute top-0 object-cover w-full h-full -z-1" style="border-bottom-right-radius: 25px; border-bottom-left-radius: 25px">
+      <img src="<?= $image->thumb(['width' => 400])->url() ?>" class="absolute top-0 object-cover w-full h-full transition-opacity duration-300 ease-in opacity-0 -z-1" :class="{ 'opacity-100': loaded }" style="border-bottom-right-radius: 25px; border-bottom-left-radius: 25px" @load="loaded = true">
     <?php else : ?>
       <div class="absolute top-0 w-full h-full bg-center bg-no-repeat bg-striped -z-1"></div>
     <?php endif; ?>
--- src/site/templates/recipe.php
+++ src/site/templates/recipe.php
@@ -36,8 +36,8 @@
       <ul class="flex mb-10 overflow-auto scrolling-touch">
         <?php foreach ($page->images() as $image) : ?>
           <li class="flex-shrink-0 py-2 pr-5 <?= e($gallery, 'w-56 first:ml-5', 'w-full pl-5') ?>">
-            <picture class="<?= e($gallery, 'aspect-ratio-tall', 'aspect-ratio-wide') ?> relative block overflow-hidden shadow rounded-card">
-              <img src="<?= $image->thumb(['width' => 900])->url() ?>" class="absolute object-cover w-full h-full">
+            <picture class="<?= e($gallery, 'aspect-ratio-tall', 'aspect-ratio-wide') ?> relative block overflow-hidden shadow rounded-card" x-data="{ loaded: false }">
+              <img src="<?= $image->thumb(['width' => 900])->url() ?>" class="absolute object-cover w-full h-full transition-opacity duration-500 ease-in opacity-0" :class="{ 'opacity-100': loaded }" @load="loaded = true">
             </picture>
           </li>
         <?php endforeach; ?>
