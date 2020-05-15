dff69fe2d20c9cb1386bbdc55e575a84213db981
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-05-15 21:13:42 +0200

    fix(compat): use normal string instead of backtick

--- src/assets/js/scrollbar.js
+++ src/assets/js/scrollbar.js
@@ -3,8 +3,8 @@
   var body = document.body
   var el = body.appendChild(doc.createElement('div'))
 
-  el.style.cssText = `
-  width:100px;height:100px;overflow:scroll !important;position:absolute;top:-100vh`
+  el.style.cssText =
+    'width:100px;height:100px;overflow:scroll !important;position:absolute;top:-100vh'
 
   var hasScrollbar = el.offsetWidth - el.clientWidth > 0
   if (hasScrollbar) document.documentElement.classList.add('has-scrollbar')
