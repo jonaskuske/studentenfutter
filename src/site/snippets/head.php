c8cddaaa0ad474425484e3692867dc979a918686
riekehieke <30436310+riekehieke@users.noreply.github.com>
2020-06-12 13:43:38 +0200

    splashscreen ios

--- src/site/snippets/head.php
+++ src/site/snippets/head.php
@@ -17,11 +17,19 @@
   ]) ?>
 
   <link rel="manifest" href="<?= asset('assets/meta/site.webmanifest')->url() ?>" />
-  <link rel="apple-touch-icon" sizes="180x180" href="<?= asset('assets/meta/apple-touch-icon.png')->url() ?>">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?= asset('assets/meta/favicon-32x32.png')->url() ?>">
-  <link rel="icon" type="image/png" sizes="16x16" href="<?= asset('assets/meta/favicon-16x16.png')->url() ?>">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= asset(
+    'assets/meta/apple-touch-icon.png',
+  )->url() ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= asset(
+    'assets/meta/favicon-32x32.png',
+  )->url() ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= asset(
+    'assets/meta/favicon-16x16.png',
+  )->url() ?>">
   <link rel="manifest" href="<?= asset('assets/meta/site.webmanifest')->url() ?>">
-  <link rel="mask-icon" href="<?= asset('assets/meta/safari-pinned-tab.svg')->url() ?>" color="#f28b85">
+  <link rel="mask-icon" href="<?= asset(
+    'assets/meta/safari-pinned-tab.svg',
+  )->url() ?>" color="#f28b85">
   <link rel="shortcut icon" href="<?= asset('assets/meta/favicon.ico')->url() ?>">
   <meta name="apple-mobile-web-app-title" content="studentenfutter">
   <meta name="application-name" content="studentenfutter">
@@ -30,6 +38,26 @@
   <meta name="theme-color" content="#F28B85" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+  <!-- Splashscreen iOS -->
+  <!-- iPhone Xs Max (1242px x 2688px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" href="assets/meta/apple-launch-1242x2688.png">
+<!-- iPhone Xr (828px x 1792px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="assets/meta/apple-launch-828x1792.png">
+<!-- iPhone X, Xs (1125px x 2436px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="assets/meta/apple-launch-1125x2436.png">
+<!-- iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus (1242px x 2208px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="assets/meta/apple-launch-1242x2208.png">
+<!-- iPhone 8, 7, 6s, 6 (750px x 1334px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="assets/meta/apple-launch-750x1334.png">
+<!-- iPad Pro 12.9" (2048px x 2732px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="assets/meta/apple-launch-2048x2732.png">
+<!-- iPad Pro 11” (1668px x 2388px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" href="assets/meta/apple-launch-1668x2388.png">
+<!-- iPad Pro 10.5" (1668px x 2224px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="assets/meta/apple-launch-1668x2224.png">
+<!-- iPad Mini, Air (1536px x 2048px) -->
+<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="assets/meta/apple-launch-1536x2048.png">
 
   <?= js(['assets/js/polyfills.js', '@auto'], ['defer' => true]) ?>
 
