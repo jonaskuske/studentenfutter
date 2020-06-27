ff6627256a94032a42f02b9e3aca8be757e66148
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-06-27 17:23:01 +0200

    fix: load fonts before regular CSS

--- src/site/snippets/head.php
+++ src/site/snippets/head.php
@@ -31,11 +31,6 @@ $img = $has_img
 
   <title><?= $title ?></title>
 
-  <?= css([
-    $kirby->option('production') ? 'assets/css/tailwind.min.css' : 'assets/css/tailwind.dev.css',
-    '@auto',
-  ]) ?>
-
   <style>
   @font-face {
     font-family: 'Chivo';
@@ -78,6 +73,11 @@ $img = $has_img
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
   </style>
+
+  <?= css([
+    $kirby->option('production') ? 'assets/css/tailwind.min.css' : 'assets/css/tailwind.dev.css',
+    '@auto',
+  ]) ?>
 
   <link rel="manifest" href="<?= asset('assets/meta/site.webmanifest')->url() ?>" />
   <link
