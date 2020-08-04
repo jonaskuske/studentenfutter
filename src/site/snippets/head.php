c78615b18f5539b6b8f0b19d2ac7dca395c2dd6b
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-08-04 23:18:39 +0200

    fix: use font-display: auto

--- src/site/snippets/head.php
+++ src/site/snippets/head.php
@@ -36,7 +36,7 @@ $img = $has_img
     font-family: 'Chivo';
     font-style: italic;
     font-weight: 300;
-    font-display: block;
+    font-display: auto;
     src: local('Chivo Light Italic'), local('Chivo-LightItalic'),
       url(<?= asset('assets/fonts/Chivo_Light-Italic.woff2')->url() ?>) format('woff2'),
       url(<?= asset('assets/fonts/Chivo_Light-Italic.woff')->url() ?>) format('woff');
@@ -46,7 +46,7 @@ $img = $has_img
     font-family: 'Chivo';
     font-style: normal;
     font-weight: 400;
-    font-display: block;
+    font-display: auto;
     src: local('Chivo Regular'), local('Chivo-Regular'),
       url(<?= asset('assets/fonts/Chivo_Regular.woff2')->url() ?>) format('woff2'),
       url(<?= asset('assets/fonts/Chivo_Regular.woff')->url() ?>) format('woff');
@@ -56,7 +56,7 @@ $img = $has_img
     font-family: 'Chivo';
     font-style: italic;
     font-weight: 400;
-    font-display: block;
+    font-display: auto;
     src: local('Chivo Italic'), local('Chivo-Italic'),
       url(<?= asset('assets/fonts/Chivo_Regular-Italic.woff2')->url() ?>) format('woff2'),
       url(<?= asset('assets/fonts/Chivo_Regular-Italic.woff')->url() ?>) format('woff');
@@ -66,7 +66,7 @@ $img = $has_img
     font-family: 'Chivo';
     font-style: italic;
     font-weight: 700;
-    font-display: block;
+    font-display: auto;
     src: local('Chivo Bold Italic'), local('Chivo-BoldItalic'),
       url(<?= asset('assets/fonts/Chivo_Bold-Italic.woff2')->url() ?>) format('woff2'),
       url(<?= asset('assets/fonts/Chivo_Bold-Italic.woff')->url() ?>) format('woff');
