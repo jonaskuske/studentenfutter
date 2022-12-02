2c2d9d17a3920f5719d55fa75680ee597571b88a
Jonas <30421456+jonaskuske@users.noreply.github.com>
2022-12-02 18:33:07 +0100

    feat: auto-show keyboard for searching

--- src/assets/js/templates/search.js
+++ src/assets/js/templates/search.js
@@ -1,4 +1,5 @@
 if (!/q=/.test(window.location.search)) {
   const input = document.querySelector('input[type="search"]')
   input && input.focus()
+  if (navigator.virtualKeyboard) navigator.virtualKeyboard.show()
 }
